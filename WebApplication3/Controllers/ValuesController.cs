b88f5a0536df498cf61d649fd12eab547e1ca582
charandeep singh <N01748813@humber.ca>
2025-05-30 14:55:41 -0400

    Add project files.

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet("Welcome")]
+        public ActionResult<String> GetWelcome()
+        {
+            return "Welcome to 5125!";
+        }
+
+        //[HttpPost]
+        //public string post(string mess)
+        //{
+        //    string Message = "This is post Request";
+        //    return Message;
+        //}
+        
+    }
+}
---
+++ WebApplication3/Controllers/ValuesController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Question2con.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController1 : ControllerBase
+    {
+        [HttpPost]
+        public string post(string mess)
+        {
+            string Message = "This is post Request";
+            return Message;
+        }
+    }
+}
